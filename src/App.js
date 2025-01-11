958df07c1d63f71c368a3b327bfe170f7ed662b1
Erry Kostala <errietta@users.noreply.github.com>
2025-01-11 11:37:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,30 @@ import LoginButton from './LoginButton';
 
 
 function App() {
+  const [isOffline, setIsOffline] = useState(!navigator.onLine);
   const { isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) {
+  useEffect(() => {
+    const handleOnline = () => setIsOffline(false);
+    const handleOffline = () => setIsOffline(true);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  if (!isOffline && isLoading) {
     return <div>Loading ...</div>;
   }
 
     return (
           <div>
             <LanguageSwitcher />
-            {isAuthenticated ? <Cards /> : <LoginButton />}
+            { (isAuthenticated || isOffline) ? <Cards /> : <LoginButton />}
           </div>
     );
 }
